dfff3ee8f55aaa00e66f78ffae4ff1a77487b2c0
Arshon Saadati <ArshonSaadati@gmail.com>
2024-08-04 21:58:28 -0700

    improved lualine to use relative file path and telesope to close buffers with C-d

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -1,5 +1,39 @@
 require('telescope').setup({ file_ignore_patterns = { "node%_modules/.*" } })
 local builtin = require('telescope.builtin')
+local action_state = require('telescope.actions.state')
+local actions = require('telescope.actions')
+
+-- Delete buffer(s) from the buffer list using Telescope
+local buffer_searcher
+buffer_searcher = function()
+    builtin.buffers {
+        sort_mru = true,
+        ignore_current_buffer = true,
+        show_all_buffers = false,
+        attach_mappings = function(prompt_bufnr, map)
+            local refresh_buffer_searcher = function()
+                actions.close(prompt_bufnr)
+                vim.schedule(buffer_searcher)
+            end
+            local delete_buf = function()
+                local selection = action_state.get_selected_entry()
+                vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+                refresh_buffer_searcher()
+            end
+            local delete_multiple_buf = function()
+                local picker = action_state.get_current_picker(prompt_bufnr)
+                local selection = picker:get_multi_selection()
+                for _, entry in ipairs(selection) do
+                    vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+                end
+                refresh_buffer_searcher()
+            end
+            map('n', '<C-d>', delete_buf)
+            map('i', '<C-d>', delete_buf)
+            return true
+        end
+    }
+end
 
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
@@ -10,24 +44,24 @@ vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]re
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader><leader>', buffer_searcher, { desc = '[ ] Find existing buffers' })
 
 -- Slightly advanced example of overriding default behavior and theme
 vim.keymap.set('n', '<leader>/', function()
--- You can pass additional configuration to Telescope to change the theme, layout, etc.
-builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-})
+    -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 -- It's also possible to pass additional configuration options.
 --  See `:help telescope.builtin.live_grep()` for information about particular keys
 vim.keymap.set('n', '<leader>s/', function()
-builtin.live_grep {
-    grep_open_files = true,
-    prompt_title = 'Live Grep in Open Files',
-}
+    builtin.live_grep {
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+    }
 end, { desc = '[S]earch [/] in Open Files' })
 
 -- Shortcut for searching your Neovim configuration files
